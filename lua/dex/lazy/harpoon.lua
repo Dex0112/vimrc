ef56daba42cbc32798295607730a469b0d3fde34
Dex0112 <dexcrazy06@gmail.com>
2024-01-21 07:02:03 -0500

    Harpoon modification

--- lua/dex/lazy/harpoon.lua
+++ lua/dex/lazy/harpoon.lua
@@ -9,7 +9,7 @@ return {
 
         vim.keymap.set("n", "<M-j>", function() ui.nav_file(1) end)
         vim.keymap.set("n", "<M-k>", function() ui.nav_file(2) end)
-        vim.keymap.set("n", "<M-h>", function() ui.nav_file(3) end)
+        vim.keymap.set("n", "<M-l>", function() ui.nav_file(3) end)
         vim.keymap.set("n", "<M-;>", function() ui.nav_file(4) end)
     end
 }
