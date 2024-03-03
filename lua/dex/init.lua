5b70e2edaaba80ae4aaf206df556ae1a8fdc674b
Dex0112 <dexcrazy06@gmail.com>
2024-03-03 08:31:42 -0500

    Feat: Autocmds

--- lua/dex/init.lua
+++ lua/dex/init.lua
@@ -1,3 +1,4 @@
 require("dex.set")
 require("dex.remap")
 require("dex.lazy_init")
+require('dex.autocmd')
--- lua/dex/lazy/lsp.lua
+++ lua/dex/lazy/lsp.lua
@@ -72,7 +72,8 @@ return {
             }
         })
 
-        lsp.on_attach(function(client, bufnr)
+---@diagnostic disable-next-line: unused-local
+       lsp.on_attach(function(client, bufnr)
             local opts = { buffer = bufnr, remap = false }
 
             vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
@@ -84,7 +85,6 @@ return {
             vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, opts)
             vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, opts)
             vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts)
-            vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
         end)
 
         lsp.setup()
