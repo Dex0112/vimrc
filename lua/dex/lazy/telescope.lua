f9a0769b8048d9cc9bb8a4879c2d475550f03482
Dex0112 <dexcrazy06@gmail.com>
2024-03-03 08:53:40 -0500

    Got rid of winblend in telescope because I don't think it does anything in my setup

--- lua/dex/lazy/telescope.lua
+++ lua/dex/lazy/telescope.lua
@@ -17,7 +17,6 @@ return {
             print("running")
 
             builtin.current_buffer_fuzzy_find(require('telescope.themes').get_dropdown {
-                winblend = 10,
                 previewer = false,
             })
         end, {})
