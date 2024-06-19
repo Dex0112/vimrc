5f7eb8d911f2eecde7084ebb2fb7cff743b2a52e
Dex0112 <dexcrazy06@gmail.com>
2024-06-19 01:48:53 -0400

    Added bufnr remaps

--- lua/dex/lazy/lsp.lua
+++ lua/dex/lazy/lsp.lua
@@ -29,6 +29,18 @@ return {
             handlers = {
                 function(server_name) -- default handler (optional)
                     require("lspconfig")[server_name].setup {
+                        on_attach = function(_, bufnr)
+                            local opts = { buffer = bufnr, remap = false }
+
+                            vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
+                            vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
+                            vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
+                            vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
+                            vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, opts)
+                            vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, opts)
+                            vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts)
+
+                        end,
                         capabilities = capabilities
                     }
                 end,
