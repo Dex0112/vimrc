b9d4e86319cff2bf4cab084f18f530184b594517
Dex0112 <dexcrazy06@gmail.com>
2024-06-20 20:50:26 -0400

    Moved autocmd.lua into init.lua & moved LspAttach stuff to an autocmd

--- lua/dex/init.lua
+++ lua/dex/init.lua
@@ -1,4 +1,26 @@
 require("dex.set")
 require("dex.remap")
 require("dex.lazy_init")
-require('dex.autocmd')
+
+vim.api.nvim_create_autocmd('TextYankPost', {
+    group = vim.api.nvim_create_augroup('highlight-yank', {}),
+    callback = function()
+        vim.highlight.on_yank()
+    end,
+})
+
+vim.api.nvim_create_autocmd('LspAttach', {
+    group = vim.api.nvim_create_augroup('lsp-keymaps', {}),
+    callback = function(e)
+        local opts = { buffer = e.buf }
+
+        vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
+        vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
+        vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
+        vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
+        vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, opts)
+        vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, opts)
+        vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts)
+    end,
+})
+
