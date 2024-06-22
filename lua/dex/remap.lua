c18577ca54850a02458aacb957019ed9a7d29c3f
Dex0112 <dexcrazy06@gmail.com>
2024-06-22 00:14:07 -0400

    Readded zz after <C-d> because it got removed by accident

--- lua/dex/remap.lua
+++ lua/dex/remap.lua
@@ -12,6 +12,7 @@ vim.keymap.set("n", "<leader>Y", [["+Y]])
 
 vim.keymap.set({ "n", "v" }, "<leader>d", [["_d]])
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
 
 vim.keymap.set("n", "<leader><leader>", function()
     vim.cmd("so")
