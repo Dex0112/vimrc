c94467b16b369d7ab8ea87f941fd5d361512c7b7
Dex0112 <dexcrazy06@gmail.com>
2025-06-28 12:15:21 -0400

    Godot support added

--- init.lua
+++ init.lua
@@ -10,3 +10,9 @@ function SetColorscheme(color)
 end
 
 SetColorscheme('mellow')
+
+if vim.fn.filereadable(vim.fn.getcwd() .. "./project.godot") == 1 then
+    local addr = "./godot.pipe"
+
+    vim.fn.serverstart(addr)
+end
