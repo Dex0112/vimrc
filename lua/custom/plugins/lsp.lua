acad99ec27f2272a1eb0408003525e4a9b82d35a
Dex0112 <dexcrazy06@gmail.com>
2026-05-28 16:20:12 -0400

    Arduino Language Server

--- lua/custom/plugins/lsp.lua
+++ lua/custom/plugins/lsp.lua
@@ -97,16 +97,6 @@ do
   --  See `:help lsp-config` for information about keys and how to configure
   ---@type table<string, vim.lsp.Config>
   local servers = {
-    -- clangd = {},
-    -- gopls = {},
-    -- pyright = {},
-    -- rust_analyzer = {},
-    --
-    -- Some languages (like typescript) have entire language plugins that can be useful:
-    --    https://github.com/pmizio/typescript-tools.nvim
-    --
-    -- But for many setups, the LSP (`ts_ls`) will work just fine
-    -- ts_ls = {},
 
     stylua = {}, -- Used to format Lua code
 
@@ -141,6 +131,14 @@ do
         Lua = {
           format = { enable = false }, -- Disable formatting (formatting is done by stylua)
         },
+      },
+    },
+
+    arduino_language_server = {
+      cmd = {
+        'arduino-language-server',
+        '-fqbn',
+        'esp32:esp32:esp32da',
       },
     },
   }
