80a870560fccd7c7fbc54f9c850d0e37f8457aca
Dex0112 <dexcrazy06@gmail.com>
2026-05-28 14:24:59 -0400

    Seperated all of the default config that I kept into multiple files

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -1,7 +1,59 @@
--- You can add your own plugins here or in other files in this directory!
---  I promise not to create any merge conflicts in this directory :)
---
--- See the kickstart.nvim README for more information
+do
+  -- I'm probably going to remove this at some point
+  -- Useful plugin to show you pending keybinds.
+  vim.pack.add { 'https://github.com/folke/which-key.nvim' }
+  require('which-key').setup {
+    -- Delay between pressing a key and opening which-key (milliseconds)
+    delay = 0,
+    icons = { mappings = vim.g.have_nerd_font },
+    -- Document existing key chains
+    spec = {
+      { '<leader>s', group = '[S]earch', mode = { 'n', 'v' } },
+      { '<leader>t', group = '[T]oggle' },
+      { '<leader>h', group = 'Git [H]unk', mode = { 'n', 'v' } }, -- Enable gitsigns recommended keymaps first
+      { 'gr', group = 'LSP Actions', mode = { 'n' } },
+    },
+  }
+
+  vim.pack.add { 'https://github.com/NMAC427/guess-indent.nvim' }
+  require('guess-indent').setup {}
+
+  if vim.g.have_nerd_font then vim.pack.add { 'https://github.com/nvim-tree/nvim-web-devicons' } end
+
+  vim.pack.add { 'https://github.com/lewis6991/gitsigns.nvim' }
+  require('gitsigns').setup {
+    signs = {
+      add = { text = '+' }, ---@diagnostic disable-line: missing-fields
+      change = { text = '~' }, ---@diagnostic disable-line: missing-fields
+      delete = { text = '_' }, ---@diagnostic disable-line: missing-fields
+      topdelete = { text = '‾' }, ---@diagnostic disable-line: missing-fields
+      changedelete = { text = '~' }, ---@diagnostic disable-line: missing-fields
+    },
+  }
+
+  -- Highlight todo, notes, etc in comments
+  vim.pack.add { 'https://github.com/folke/todo-comments.nvim' }
+  require('todo-comments').setup { signs = false }
+
+  vim.pack.add {
+    'https://github.com/nvim-tree/nvim-web-devicons',
+    'https://github.com/nvim-lualine/lualine.nvim',
+  }
+
+  require('lualine').setup {
+    sections = {
+      lualine_a = { 'mode' },
+      lualine_b = { 'branch', 'diagnostics' },
+      lualine_c = { 'filename' },
+      lualine_x = { 'fileformat', 'filetype' },
+      lualine_y = {},
+      lualine_z = { 'location' },
+    },
+  }
+
+  vim.pack.add { { src = 'https://github.com/L3MON4D3/LuaSnip', version = vim.version.range '2.*' } }
+  require('luasnip').setup {}
+end
 
 -- Iterate over all Lua files in the plugins directory and load them
 local plugins_dir = vim.fs.joinpath(vim.fn.stdpath 'config', 'lua', 'custom', 'plugins')
